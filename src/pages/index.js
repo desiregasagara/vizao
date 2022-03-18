7baeec23392472b254b58c562e0b05333e46450f
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2022-03-18 15:41:19 +0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,27 @@ import QuoteBar from '../components/QuoteBar'
 import Portfolio from '../components/Portfolio'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
-
+import { Link } from 'gatsby'
 const IndexPage = () => {
 
   return(
     <div id="home">
-    <NavBar/>
-    <IntroBlock/>
-    <About/>
-    <QuoteBar/>
-    <Portfolio/>
-    <Contact/>
-    <Footer/>
+      <head>
+        <Link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png"></Link>
+        <Link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png"></Link>
+        <Link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png"></Link>
+        <Link rel="manifest" href="../images/site.webmanifest"></Link>
+        <Link rel="mask-icon" href="../images/safari-pinned-tab.svg" color="#5bbad5"></Link>
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#000"></meta>
+      </head>
+      <NavBar/>
+      <IntroBlock/>
+      <About/>
+      <QuoteBar/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
     </div>
    
   )
