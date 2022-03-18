9deeb22bfac2e9f75ddb9e7b84b40183a4c38ded
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2022-03-18 17:58:24 +0300

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@ module.exports = {
     plugins: [
       'gatsby-plugin-sass', "gatsby-plugin-image",
       "gatsby-plugin-sharp",`gatsby-plugin-styled-components`,
-     `gatsby-plugin-anchor-links` ,`gatsby-plugin-netlify`
+     `gatsby-plugin-anchor-links` ,`gatsby-plugin-netlify`,`gatsby-plugin-offline`,
+     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Vizao`,
+        short_name: `Vizao`,
+        start_url: `/`,
+        background_color: `#000`,
+        theme_color: `#000`,
+        display: `standalone`,
+        icon: `src/images/Vizao Logo Linkedin.png`
+      },
+    },
     ]
 }
