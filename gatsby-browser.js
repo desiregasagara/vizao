2601e4c6a34e7f42426ed86afc8cf27bfed44c32
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2022-03-25 15:15:37 +0300

    Add files via upload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,17 +2,3 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@popperjs/core/dist/umd/popper.min.js";
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-    if (stage === "build-html" || stage === "develop-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /bad-module/,
-              use: loaders.null(),
-            },
-          ],
-        },
-      })
-    }
-  }
--- gatsby-config.js
+++ gatsby-config.js
@@ -21,17 +21,3 @@ module.exports = {
     },
     ]
 }
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html" || stage === "develop-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /bad-module/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
