f7cb71fc3c5401df17b95f71a70803bf7a48344f
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2022-03-25 14:55:22 +0300

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,3 +21,17 @@ module.exports = {
     },
     ]
 }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
