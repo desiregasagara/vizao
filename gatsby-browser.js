ba06b0f50452639470c362747f9290fac6edbc51
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2022-03-25 17:09:03 +0300

    Add files via upload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@popperjs/core/dist/umd/popper.min";
 
