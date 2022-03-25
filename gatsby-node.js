2852d69d476e3b834c06aee8a5815972043e1424
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2022-03-25 15:53:30 +0300

    Add files via upload

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
         module: {
           rules: [
             {
-              test: /react-dom/,
+              test: /bootstrap/,
               use: loaders.null(),
             },
           ],
