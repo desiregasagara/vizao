0f4d6fb9ecdb094a9b510f1da57e5ce7e800b34d
desiregasagara <56978156+desiregasagara@users.noreply.github.com>
2022-03-29 20:02:07 +0300

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,10 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "UA-224149803-1", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        
         ],
         // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
+        // This config will be shared acrosgs all trackingIds
         gtagConfig: {
           optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
@@ -37,8 +36,9 @@ module.exports = {
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
+          anonymize_ip: true,
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
